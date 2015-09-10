d6b6e2a7cbb658c1b0278312055e708b70769a27
viagrawal <viagrawal@expedia.com>
2015-09-10 10:19:46 +0530

    Initial commit

---
+++ src/main/java/hello/Application.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author viagrawal
+ *
+ */
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String args[]) {
+
+		SpringApplication.run(Application.class, args);
+
+	}
+
+}
---
+++ src/main/java/hello/Greeting.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package hello;
+
+/**
+ * @author viagrawal
+ *
+ */
+public class Greeting {
+
+	
+	private final long id;
+    private final String content;
+
+    public Greeting(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+	
+	
+}
